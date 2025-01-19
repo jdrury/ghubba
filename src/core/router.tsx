56f03944bee13afb384201e3ee925bf2f247f62e
James Drury <5431100+jdrury@users.noreply.github.com>
2025-01-18 17:35:49 -0800

    remove deleted file from router

--- src/core/router.tsx
+++ src/core/router.tsx
@@ -1,13 +1,11 @@
 import { Route, Switch } from "wouter";
 
 import { HomeContainer } from "@/feature/home/home-container.tsx";
-import { PokemonDetailContainer } from "@/feature/pokemon-detail/pokemon-detail-container.tsx";
 
 export function Router() {
   return (
     <Switch>
       <Route path="/" component={HomeContainer} />
-      <Route path="/pokemon/:pokedexId" component={PokemonDetailContainer} />
     </Switch>
   );
 }
