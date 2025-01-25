8477e11ae2f5b8ccea00fad9e8c9bf17781abe45
James Drury <5431100+jdrury@users.noreply.github.com>
2025-01-25 14:37:52 -0800

    boilerplate for github app

---
+++ server/deps.ts
@@ -0,0 +1,3 @@
+export { serve } from "https://deno.land/std@0.204.0/http/server.ts";
+export { load } from "https://deno.land/std@0.204.0/dotenv/mod.ts";
+export { createNodeMiddleware, App } from "https://esm.sh/octokit@3.1.0";
