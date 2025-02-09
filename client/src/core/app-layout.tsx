509d5bec93f5205e9eb8d75bda8b6b5ed2f367d7
Andoni Arbulu Lozano <andoni0305@gmail.com>
2025-02-09 14:22:49 +1300

    chore(prettier): add pretteerrc bullshit

--- client/src/core/app-layout.tsx
+++ client/src/core/app-layout.tsx
@@ -3,11 +3,10 @@ import { Outlet } from "react-router";
 function AppLayout() {
   return (
     <main>
-      <nav className="h-14 border-b">
-      </nav>
+      <nav className="h-14 border-b"></nav>
       <Outlet />
     </main>
-  )
+  );
 }
 
-export { AppLayout }
+export { AppLayout };
