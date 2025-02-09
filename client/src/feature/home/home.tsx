b6bbefda2a49d0ade675e66f9e13b6807636a540
James Drury <5431100+jdrury@users.noreply.github.com>
2025-02-09 15:22:40 -0800

    show list of repos in home

--- client/src/feature/home/home.tsx
+++ client/src/feature/home/home.tsx
@@ -5,11 +5,12 @@ import {
   usePreloadedQuery,
   VariablesOf,
 } from "react-relay";
-import { useLoaderData } from "react-router";
+import { Outlet, useLoaderData } from "react-router";
 
 import { environment } from "@/lib/relay/environment.ts";
 
 import { homeQuery } from "__generated__/homeQuery.graphql";
+import { RepositoryList } from "@/feature/repository-list.tsx";
 
 const query = graphql`
   query homeQuery {
@@ -18,6 +19,7 @@ const query = graphql`
       login
       avatarUrl
       websiteUrl
+      ...repositoryListFragment
     }
   }
 `;
@@ -28,35 +30,26 @@ function loader(vars: VariablesOf<homeQuery>) {
 
 function Home() {
   const queryRef = useLoaderData<PreloadedQuery<homeQuery>>();
-
   const data = usePreloadedQuery<homeQuery>(query, queryRef);
 
-  console.log("GAAD!!");
-
   return (
-    <main>
-      <h1 className="text-4xl font-extrabold">Home</h1>
-      <div className="border-2 border-black px-2 py-5">
-        <img src={data.viewer.avatarUrl} />
-        <p>
-          <strong>@{data.viewer.login}</strong> | {data.viewer.name}
-        </p>
-      </div>
-      {/*<ul>*/}
-      {/*  {repositories?.map((repo) => (*/}
-      {/*    <li key={repo?.id}>*/}
-      {/*      <Link to={`/${data.viewer.login}/${repo?.name}`}>*/}
-      {/*        {repo?.name} ({repo?.visibility})*/}
-      {/*      </Link>*/}
-      {/*    </li>*/}
-      {/*  ))}*/}
-      {/*</ul>*/}
-      <footer>
-        <form action="/api/logout" method="POST">
-          <button type="submit">Logout</button>
-        </form>
-      </footer>
-    </main>
+    <section className="px-2 mt-6">
+      <header>
+        <h2 className="text-4xl font-extrabold">Home</h2>
+      </header>
+      <section className="flex gap-8 mt-6">
+        <section>
+          <div className="border-2 border-black px-2 py-5">
+            <img src={data.viewer.avatarUrl} />
+            <p>
+              <strong>@{data.viewer.login}</strong> | {data.viewer.name}
+            </p>
+          </div>
+        </section>
+        <RepositoryList fragmentRef={data.viewer} />
+        <Outlet />
+      </section>
+    </section>
   );
 }
 
