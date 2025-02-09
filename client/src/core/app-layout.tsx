f95463c331e31a838676062e3f9f973390bec354
James Drury <5431100+jdrury@users.noreply.github.com>
2025-02-09 15:22:08 -0800

    fix up the nav, add logout to it

--- client/src/core/app-layout.tsx
+++ client/src/core/app-layout.tsx
@@ -4,8 +4,13 @@ import { Outlet } from "react-router";
 function AppLayout() {
   return (
     <main>
-      <nav className="h-14 border-b bg-red-300"></nav>
-      <Suspense fallback={<p> loading...</p>}>
+      <nav className="h-14 px-2 flex justify-between bg-blue-400 items-center">
+        <h1>ghubba</h1>
+        <form action="/api/logout" method="POST">
+          <button type="submit">Logout</button>
+        </form>
+      </nav>
+      <Suspense fallback={<p>loading...</p>}>
         <Outlet />
       </Suspense>
     </main>
