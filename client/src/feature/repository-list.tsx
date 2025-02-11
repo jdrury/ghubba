edc0eb958c96e52af860d2e9abf04954c0442cfc
James Drury <5431100+jdrury@users.noreply.github.com>
2025-02-11 07:04:20 -0800

    move connection into fragment to get rid of the crazy type guard

--- client/src/feature/repository-list.tsx
+++ client/src/feature/repository-list.tsx
@@ -4,12 +4,16 @@ import { graphql, useFragment } from "react-relay";
 import { repositoryListFragment$key } from "__generated__/repositoryListFragment.graphql.ts";
 
 const fragment = graphql`
-  fragment repositoryListFragment on Repository @relay(plural: true) {
-    id
-    name
-    description
-    owner {
-      login
+  fragment repositoryListFragment on RepositoryConnection {
+    edges {
+      node {
+        id
+        name
+        description
+        owner {
+          login
+        }
+      }
     }
   }
 `;
@@ -20,17 +24,18 @@ type Props = {
 
 function RepositoryList({ fragmentRef }: Props) {
   const data = useFragment(fragment, fragmentRef);
+  const repositories = data.edges?.map((edge) => edge?.node);
   return (
     <section>
-      {data.map((repo) => (
-        <li key={repo.id}>
+      {repositories?.map((repo) => (
+        <li key={repo?.id}>
           <Link
             className="text-blue-500"
-            to={`/${repo.owner.login}/${repo.name}`}
+            to={`/${repo?.owner.login}/${repo?.name}`}
           >
-            {repo.name}
+            {repo?.name}
           </Link>
-          <small>{repo.description ?? "<no description>"}</small>
+          <small>{repo?.description ?? "<no description>"}</small>
         </li>
       ))}
     </section>
--- client/src/feature/user-detail/user-detail.tsx
+++ client/src/feature/user-detail/user-detail.tsx
@@ -9,10 +9,7 @@ import { LoaderFunctionArgs, useLoaderData } from "react-router";
 import { environment } from "@/lib/relay/environment";
 import { RepositoryList } from "@/feature/repository-list.tsx";
 
-import {
-  userDetailQuery,
-  userDetailQuery$data,
-} from "__generated__/userDetailQuery.graphql";
+import { userDetailQuery } from "__generated__/userDetailQuery.graphql";
 
 const query = graphql`
   query userDetailQuery($login: String!) {
@@ -20,28 +17,12 @@ const query = graphql`
       name
       login
       avatarUrl
-      topRepositories(
-        first: 25
-        orderBy: { direction: DESC, field: UPDATED_AT }
-      ) {
-        edges {
-          node {
-            ...repositoryListFragment
-          }
-        }
+      repositories(first: 25, orderBy: { direction: DESC, field: UPDATED_AT }) {
+        ...repositoryListFragment
       }
     }
   }
 `;
-
-// Extract a type from graphql file for our guard ("repo is TopRepository")
-type TopRepository = NonNullable<
-  NonNullable<
-    NonNullable<
-      NonNullable<userDetailQuery$data["user"]>["topRepositories"]["edges"]
-    >[number]
-  >["node"]
->;
 
 function loadUser({ params }: LoaderFunctionArgs) {
   const vars = {
@@ -58,10 +39,6 @@ function UserDetail() {
     return <h2>404: User not found</h2>;
   }
 
-  const topRepositories = data.user.topRepositories.edges
-    ?.map((edge) => edge?.node)
-    .filter((repo): repo is TopRepository => Boolean(repo));
-
   return (
     <section>
       <div className="border-2 border-black px-2 py-5">
@@ -73,9 +50,7 @@ function UserDetail() {
           <strong>@{data.user.login}</strong> | {data.user.name}
         </p>
       </div>
-      {topRepositories != null && (
-        <RepositoryList fragmentRef={topRepositories} />
-      )}
+      <RepositoryList fragmentRef={data.user.repositories} />
     </section>
   );
 }
